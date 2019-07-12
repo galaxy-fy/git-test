fce8bfee1c8a49ce91c96a33730ca92ec8bf463f
admin <admin@163.com>
2019-07-12 19:09:17 +0800

    将本地项目添加到git

---
+++ GitProject/src/gjx/fy/GitTest.java
@@ -0,0 +1,12 @@
+package gjx.fy;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
